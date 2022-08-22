18f6ef7b1951bd674dd10aed123c6994db4bb230
cappochinho <edwardprempeh18@gmail.com>
2022-08-22 09:36:08 -0700

    Technical interview, singly linked lists

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if a singly linked list has a cycle in it
+ * @list: singly linked list
+ * Return: 0 if there is no cycle, else 1 if there is a cycle
+ *
+ */
+int check_cycle(listint_t *list)
+{
+	int n_cycles = 0;
+	listint_t *temp_list = list;
+
+	while (temp_list != NULL)
+	{
+		temp_list = temp_list->next;
+		if (list == temp_list)
+		{
+			n_cycles = 1;
+			break;
+		}
+	}
+
+	return (n_cycles);
+}
