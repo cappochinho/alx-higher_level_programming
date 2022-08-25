e97f7a99fea5576a527b89e0be7a51e1e13815c9
cappochinho <edwardprempeh18@gmail.com>
2022-08-25 07:09:31 -0700

    Taking arguments from cmd

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    l = len(argv)
-    if l == 2:
-        print("{:d} argument:".format(l - 1))
-        print("{:d}: {:s}".format(l - 1, argv[l - 1]))
-    else:
-        if l == 1:
-            print("{:d} arguments.".format(l - 1))
-        else:
-            print("{:d} arguments:".format(l - 1))
-            i = 1
-            while (i <= l - 1):
-                print("{:d}: {:s}".format(i, argv[i]))
-                i += 1
+    l = len(argv) - 1
+    print("{:d} {:s}{:s}".format(l, "argument" if l == 1 else "arguments",
+                                 "." if l == 0 else ":"))
+    for d, s in enumerate(argv):
+        if d > 0:
+            print("{:d}: {:s}".format(d, s))
