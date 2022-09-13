67e7e598c12ebd8d7e852623c602974ac64a3942
cappochinho <65264688+cappochinho@users.noreply.github.com>
2022-09-13 22:09:06 +0000

    Update 3-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,15 +1,30 @@
 #!/usr/bin/python3
+"""Square module."""
+
 
 class Square:
-    """Square() class with public instance method"""
-    def __init__(self, size=0):
-        if type(size) is not int:
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
-            
-    def area(self):
-        return self.__size ** 2
+    """Defines a square."""
 
+    def __init__(self, size=0):
+        """Constructor.
+
+        Args:
+            size: length of side of the square.
+
+        Raises:
+            TypeError: if size is not an integer.
+            ValueError: If size < 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Area of the square.
+
+        Returns:
+            thee size squared.
+        """
+        return self.__size ** 2
