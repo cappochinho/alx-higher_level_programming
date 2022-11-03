7fb96d248988b2427352d1369690dad3308291fb
cappochinho <edwardprempeh18@gmail.com>
2022-11-03 21:55:55 +0000

    5, 6, 7

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,2 +1,2 @@
--- Create table with defaul/unique id field
+-- Create table with defaulf/unique id field
 CREATE TABLE IF NOT EXISTS unique_id (id INT DEFAULT 1 UNIQUE, name VARCHAR(256));
---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,9 @@
+--create database and table cities
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities (
+    id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    state_id INT NOT NULL,
+    CONSTRAINT FOREIGN_KEY (state_id) REFERENCES states(id) ON UPDATE RESTRICT,
+    name VARCHAR(256) NOT NULL
+    );
