ca1eb09a54b186c1cb8f39c856d2534a83f9302b
cappochinho <edwardprempeh18@gmail.com>
2022-12-03 07:00:43 -0800

    Still on it

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# Lists all cities of the database hbtn_0e_4_usa, ordered by city id.
+# Usage: ./4-cities_by_state.py <mysql username> \
+#                               <mysql password> \
+#                               <database name>
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    list = sys.argv
+    username, password, database = (list[1], list[2], list[3])
+    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
+    c = db.cursor()
+    c.execute("SELECT `c`.`id`, `c`.`name`, `s`.`name` \
+                 FROM `cities` as `c` \
+                INNER JOIN `states` as `s` \
+                   ON `c`.`state_id` = `s`.`id` \
+                ORDER BY `c`.`id`")
+    [print(city) for city in c.fetchall()]
