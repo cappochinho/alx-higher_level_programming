21362a644893ccf5730f2d3529072b7c17fc8d76
cappochinho <edwardprempeh18@gmail.com>
2022-12-03 04:27:36 -0800

    First commit

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+# Lists all states from the database hbtn_0e_0_usa
+# Usage: ./0-select_states.py <mysql username> \
+#                             <mysql password> \
+#                             <database name>
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+
+    list = sys.argv
+    username, password, database = (list[1], list[2], list[3])
+    connection = MySQLdb.connect(host = "localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=database)
+
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    query_rows = cursor.fetchall()
+    for row in query_rows:
+        print(row)
+    cursor.close()
+    connection.close()
---
+++ 0x0F-python-object_relational_mapping/README.md
@@ -0,0 +1,2 @@
+# ORM Python-SQL
+
